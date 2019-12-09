32d83038303df117aaa9065bb83723c14f6cac53
grantpickers <grantpickers@users.noreply.github.com>
2019-12-09 14:28:03 +1100

    new layout fuck that old shit

--- css/changer.js
+++ css/changer.js
@@ -9,3 +9,14 @@ function closeNav() {
   document.getElementById("main").style.marginLeft= "0";
 }
 
+const imgContent = document.querySelectorAll('.img-content-hover');
+
+function showImgContent(e) {
+  for(var i = 0; i < imgContent.length; i++) {
+    imgContent[i].style.left = e.pageX + 'px';
+    imgContent[i].style.top = e.pageY + 'px';
+  }
+};
+
+document.addEventListener('mousemove', showImgContent);
+
