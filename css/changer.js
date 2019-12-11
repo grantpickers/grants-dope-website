2435a5690b7c21a69725411ab8d1e06e64e18449
grantpickers <grantpickers@users.noreply.github.com>
2019-12-12 10:31:17 +1100

    save

--- css/changer.js
+++ css/changer.js
@@ -1,11 +1,13 @@
-
-function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
-}
-
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
   document.getElementById("main").style.marginLeft= "0";
 }
 
+const imgContent = document.querySelectorAll('.img-content-hover');
+
+function showImgContent(e) {
+  for(var i = 0; i < imgContent.length; i++) {
+    imgContent[i].style.left = e.pageX + 'px';
+    imgContent[i].style.top = e.pageY + 'px';
+  }
+};
+
+document.addEventListener('mousemove', showImgContent);
